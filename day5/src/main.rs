44c4a66ebf81e23939a0875966f8df1ef9c83be4
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2018-12-05 15:08:34 +0100

    Clippy fix

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,7 +11,7 @@ fn opposite(c1: char, c2: char) -> bool {
     c1 != c2 && c1.eq_ignore_ascii_case(&c2)
 }
 
-fn shorten(chain: String) -> String {
+fn shorten(chain: &str) -> String {
     chain.chars().fold("".to_string(), |mut sum, c| {
         if !sum.is_empty() && opposite(c, sum.chars().last().unwrap()) {
             sum.pop();
@@ -26,15 +26,15 @@ fn main() {
     let lines = read_input();
     assert!(lines.len() == 1);
 
-    let chain = lines[0].to_string();
-    let result = shorten(chain.clone());
+    let chain = &lines[0];
+    let result = shorten(chain);
 
     println!("{}", result.len());
 
     let mut best = chain.len();
     for c in ALPHA.chars() {
         let new = chain.replace(c, "").replace(&c.to_uppercase().to_string(), "");
-        let shortened = shorten(new);
+        let shortened = shorten(&new);
         if shortened.len() < best {
             best = shortened.len();
         }
